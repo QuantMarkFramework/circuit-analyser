3d79cbb09088631556652e2c45c7018f7137b46e
Becker Leo <leijonamieli@hotmail.fi>
2021-12-17 01:15:37 +0200

    Added own implementation of square grid.

--- analyser/architectures/square_grid.py
+++ analyser/architectures/square_grid.py
@@ -1,8 +1,9 @@
-from pytket.routing import SquareGrid
+import typing
+from pytket.routing import Architecture
 import math
 
 
-def square_grid(qubits: int = 0, n: int = 0):
+def square_grid(qubits: int = 0, n: int = 0, aslist: bool = False):
 	"""
 	Creates a n * n square grid with enough qubits. You can give qubit count or
 	n as a parameter.
@@ -13,4 +14,18 @@ def square_grid(qubits: int = 0, n: int = 0):
 		raise ValueError("Must give either qubits count or n.")
 	if qubits:
 		n = math.ceil(math.sqrt(qubits))
-	return SquareGrid(n, n)
+	connections: typing.List[typing.Tuple[int, int]] = []
+
+	for i in range(n - 1):
+		connections.append((i, i + 1))
+	for row in range(1, n):
+		offset: int = row * n
+		for i in range(n - 1):
+			connections.append((offset + i, offset + i + 1))
+			connections.append((offset + i - n, offset + i))
+		connections.append((offset - 1, offset + n - 1))
+
+	if aslist:
+		return connections
+
+	return Architecture(connections)
---
+++ tests/architectures/test_square_grid.py
@@ -0,0 +1,28 @@
+import unittest
+from analyser.architectures import square_grid
+
+
+class TestSquareGrid(unittest.TestCase):
+	def test_1_qubit(self):
+		empty = []
+		self.assertEqual(sorted(square_grid(1, aslist=True)), sorted(empty))
+
+	def test_3_qubits(self):
+		con = [(0, 1), (2, 3), (0, 2), (1, 3)]
+		self.assertEqual(sorted(square_grid(qubits=3, aslist=True)), sorted(con))
+
+	def test_16_qubits(self):
+		row12 = [(0, 1), (1, 2), (2, 3), (4, 5), (5, 6), (6, 7)]
+		row23 = [(8, 9), (9, 10), (10, 11), (12, 13), (13, 14), (14, 15)]
+		col12 = [(0, 4), (4, 8), (8, 12), (1, 5), (5, 9), (9, 13)]
+		col34 = [(2, 6), (6, 10), (10, 14), (3, 7), (7, 11), (11, 15)]
+		correct = [*row12, *row23, *col12, *col34]
+		self.assertEqual(sorted(square_grid(16, aslist=True)), sorted(correct))
+
+	def test_n_as_parameter(self):
+		row12 = [(0, 1), (1, 2), (2, 3), (4, 5), (5, 6), (6, 7)]
+		row23 = [(8, 9), (9, 10), (10, 11), (12, 13), (13, 14), (14, 15)]
+		col12 = [(0, 4), (4, 8), (8, 12), (1, 5), (5, 9), (9, 13)]
+		col34 = [(2, 6), (6, 10), (10, 14), (3, 7), (7, 11), (11, 15)]
+		correct = [*row12, *row23, *col12, *col34]
+		self.assertEqual(sorted(square_grid(n=4, aslist=True)), sorted(correct))
