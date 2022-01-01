830c3d6798835d346c008ca70fa27880a7ddb445
Becker Leo <leijonamieli@hotmail.fi>
2022-01-01 21:07:30 +0200

    Changed optimisation pass

--- analyser/CircuitAnalytics.py
+++
@@ -1,9 +0,0 @@
-import typing
-
-
-class CircuitAnalytics:
-	def __init__(self, results: typing.List[typing.Dict[str, int]]):
-		self.results = results
-
-	def __str__(self):
-		return "\n".join([str(result) for result in self.results])
--- analyser/analyse.py
+++ analyser/analyse.py
@@ -1,12 +1,11 @@
-from analyser.CircuitAnalytics import CircuitAnalytics
 from dataclasses import dataclass
-import typing
 import tequila as tq
 from analyser.translate import tequila_to_tket
-from pytket.routing import Architecture
-from pytket.passes import FullPeepholeOptimise, DefaultMappingPass, RebaseTket
-from pytket.passes import SequencePass, DecomposeBoxes
+from pytket.routing import Architecture, GraphPlacement, LinePlacement
+from pytket.passes import FullPeepholeOptimise, RebaseTket, RoutingPass, PlacementPass
+from pytket.passes import SequencePass, DecomposeBoxes, DelayMeasures, RemoveRedundancies
 from pytket.predicates import CompilationUnit, GateSetPredicate, ConnectivityPredicate
+from pytket.predicates import NoMidMeasurePredicate
 from pytket.circuit import OpType
 from functools import reduce
 import random
@@ -18,7 +17,7 @@ class AnalyseError(Exception):
 
 
 @dataclass
-class Result:
+class CircuitAnalytics:
 	qubit_count: int
 	gate_depth: int
 	gate_count: int
@@ -31,44 +30,65 @@ def cx_counter(count, gate):
 	return count
 
 
+def create_pass(
+	architecture: Architecture = None,
+	placement_type: str = "graph",
+	graph_placement_timeout: int = 120000,
+):
+	"""
+	placement options "graph" "linear"
+	"""
+	passes = [DecomposeBoxes(), RebaseTket(), FullPeepholeOptimise()]
+
+	if architecture:
+		if not isinstance(placement_type, str):
+			raise ValueError("placement has to be given as 'graph' or 'linear'.")
+		elif placement_type.lower() == "graph":
+			placement = GraphPlacement(architecture)
+			placement.modify_config(timeout=graph_placement_timeout)
+		elif placement_type.lower() == "linear":
+			placement = LinePlacement(architecture)
+		else:
+			raise ValueError("placement has to be given as 'graph' or 'linear'.")
+		passes.append(PlacementPass(placement))
+		passes.append(RoutingPass(architecture))
+
+	passes.append(DelayMeasures())
+	passes.append(RebaseTket())
+	passes.append(RemoveRedundancies())
+
+	return SequencePass(passes)
+
+
 def analyse(
 	circuit: tq.QCircuit,
 	architecture: Architecture = None,
 	give_values: bool = False,
-	runs: int = 1
+	placement_type: str = "graph",
+	graph_placement_timeout: int = 120000,
 ):
-	results: typing.List[Result] = []
 	if not give_values:
 		tket_circuit = tequila_to_tket(circuit, compile=True)
-	if architecture:
-		seqpass = SequencePass([
-			DecomposeBoxes(),
-			FullPeepholeOptimise(),
-			DefaultMappingPass(architecture),
-			RebaseTket(),
-		])
-	else:
-		seqpass = SequencePass([
-			DecomposeBoxes(),
-			FullPeepholeOptimise(),
-			RebaseTket(),
-		])
-	for _ in range(runs):
-		if give_values:
-			variables = {v: random.uniform(0, 2 * np.pi) for v in circuit.extract_variables()}
-			tket_circuit = tequila_to_tket(circuit, variables, compile=True)
-		cu = CompilationUnit(tket_circuit)
-		seqpass.apply(cu)
-		outcome = cu.circuit
-		if not GateSetPredicate({OpType.CX, OpType.TK1}).verify(outcome):
-			raise AnalyseError("Resulting circuit has wrong gates. Most likely not a user error.")
-		if architecture and not ConnectivityPredicate(architecture).verify(outcome):
-			raise AnalyseError("Resulting circuit has wrong gates. Most likely not a user error.")
-		results.append(Result(
-			qubit_count=outcome.n_qubits,
-			gate_depth=outcome.depth_by_type(OpType.CX),
-			gate_count=reduce(cx_counter, outcome, 0),
-			parameter_count=len(list(circuit.make_parameter_map().keys())),
-		))
-
-	return CircuitAnalytics(results)
+	if give_values:
+		variables = {v: random.uniform(0, 2 * np.pi) for v in circuit.extract_variables()}
+		tket_circuit = tequila_to_tket(circuit, variables, compile=True)
+	cu = CompilationUnit(tket_circuit)
+	create_pass(
+		architecture=architecture,
+		placement_type=placement_type,
+		graph_placement_timeout=graph_placement_timeout
+	).apply(cu)
+	outcome = cu.circuit
+	if not GateSetPredicate({OpType.CX, OpType.TK1}).verify(outcome):
+		raise AnalyseError("Resulting circuit has wrong gates. Most likely not a user error.")
+	if architecture and not ConnectivityPredicate(architecture).verify(outcome):
+		raise AnalyseError("Resulting circuit has wrong gates. Most likely not a user error.")
+	if not NoMidMeasurePredicate().verify(outcome):
+		raise AnalyseError("Resulting circuit has mid measures. Most likely not a user error.")
+	print(list(outcome))
+	return CircuitAnalytics(
+		qubit_count=outcome.n_qubits,
+		gate_depth=outcome.depth_by_type(OpType.CX),
+		gate_count=reduce(cx_counter, outcome, 0),
+		parameter_count=len(list(circuit.make_parameter_map().keys())),
+	)
