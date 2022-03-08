b018abf59b8dfd70b867356e52cc8f83da719c25
Leo Becker <leijonamieli@hotmail.fi>
2022-03-08 20:03:15 +0200

    Changed to use pytket 0.19.2

--- analyser/architectures/fully_connected.py
+++ analyser/architectures/fully_connected.py
@@ -1,5 +1,6 @@
-from pytket.routing import FullyConnected
-
-
 def fully_connected(qubits: int, *args, **kwargs):
-	return FullyConnected(qubits)
+	"""
+	This function returns the value False. Use like any other function that
+	creates and architecture when using analyse.
+	"""
+	return False
