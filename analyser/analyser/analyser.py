37ea961015890d24d4ea2dadb65501a63efd0338
Le0nerdo <leo.becker@helsinki.fi>
2023-01-23 00:23:30 +0200

    made the analyzer record transpiling time

--- analyser/analyser/analyser.py
+++ analyser/analyser/analyser.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+import time
 
 
 @dataclass
 class CircuitAnalytics:
 	gate_count: int
 	gate_depth: int
+	transpile_time: int
 
 
 class Analyser(ABC):
@@ -32,41 +34,59 @@ class Analyser(ABC):
 	def _post_routing_optimization(self, circuit):
 		pass
 
-	def _routing_and_measuring(self, circuit, architecture):
+	def _routing_and_measuring(self, circuit, architecture, pre_time):
+		complete_time = pre_time
 		if architecture:
 			if self.ignore_errors:
 				try:
+					start_time = time.time()
 					circuit = self._routing(circuit, architecture)
+					complete_time += time.time() - start_time
 				except Exception:
-					return CircuitAnalytics(None, None)
+					return CircuitAnalytics(None, None, None)
 			else:
+				start_time = time.time()
 				circuit = self._routing(circuit, architecture)
+				complete_time += time.time() - start_time
 
 		if self.ignore_errors:
 			try:
+				start_time = time.time()
 				circuit = self._post_routing_optimization(circuit)
+				complete_time += time.time() - start_time
 			except Exception:
-				return CircuitAnalytics(None, None)
+				return CircuitAnalytics(None, None, None)
 		else:
+			start_time = time.time()
 			circuit = self._post_routing_optimization(circuit)
+			complete_time += time.time() - start_time
 
 		return CircuitAnalytics(
 			gate_count=self._get_gate_count(circuit),
 			gate_depth=self._get_gate_depth(circuit),
+			transpile_time=complete_time
 		)
 
 	def analyse(self, circuit, architecture) -> CircuitAnalytics:
+		pre_time = 0
 		if self.ignore_errors:
 			try:
+				start_time = time.time()
 				circuit = self._pre_routing_optimization(circuit)
+				pre_time = time.time() - start_time
 			except Exception:
 				if isinstance(architecture, list):
-					return [CircuitAnalytics(None, None) for _ in architecture]
-				return CircuitAnalytics(None, None)
+					return [CircuitAnalytics(None, None, None) for _ in architecture]
+				return CircuitAnalytics(None, None, None)
 		else:
+			start_time = time.time()
 			circuit = self._pre_routing_optimization(circuit)
+			pre_time = time.time() - start_time
 
 		if isinstance(architecture, list):
-			return [self._routing_and_measuring(circuit.copy(), arc) for arc in architecture]
+			return [
+				self._routing_and_measuring(circuit.copy(), arc, pre_time)
+				for arc in architecture
+			]
 
-		return self._routing_and_measuring(circuit, architecture)
+		return self._routing_and_measuring(circuit, architecture, pre_time)
--- example.py
+++ example.py
@@ -1,1 +1,71 @@
-# TODO
+from analyser.architectures import fully_connected, linear, circle, square_grid
+from analyser.architectures import octagons, hexagons, minimum_viable, qubits_to_connections
+from analyser.extra import random_CX_circuit
+from analyser.analyser import PytketAnalyzer, QiskitAnalyzer
+import random
+
+
+# Parameters
+lattice_qubit_count = 500
+biggest_qubit_count = 40
+runs = 2
+random_seed = 69
+
+qiskit_methods = ["None"]
+pytket_methods = ["linear", "graph"]
+qubit_counts = range(20, biggest_qubit_count + 20, 20)
+
+arcfs = [
+	fully_connected,
+	lambda _: linear(lattice_qubit_count),
+	circle,
+	lambda _: minimum_viable(square_grid, lattice_qubit_count),
+	lambda _: minimum_viable(octagons, lattice_qubit_count),
+	lambda _: minimum_viable(hexagons, lattice_qubit_count),
+	lambda _: minimum_viable(lambda n: qubits_to_connections(square_grid(n=n)), lattice_qubit_count),
+	lambda _: minimum_viable(lambda n: qubits_to_connections(octagons(n=n)), lattice_qubit_count),
+	lambda _: minimum_viable(lambda n: qubits_to_connections(hexagons(n=n)), lattice_qubit_count),
+]
+
+names = [
+	"fully connected",
+	"linear",
+	"circle",
+	"square",
+	"square-octagon",
+	"hexagon",
+	"heavy-square",
+	"heavy-square-octagon",
+	"heavy-hexagon"
+]
+
+print("qubits", end="")
+for method in qiskit_methods + pytket_methods:
+	print(",", end="")
+	for name in names[:-1]:
+		print(f'{method}_{name}_depth', end=",")
+		print(f'{method}_{name}_count', end=",")
+		print(f'{method}_{name}_time', end=",")
+	print(f'{method}_{names[-1]}_depth', end=",")
+	print(f'{method}_{names[-1]}_count', end=",")
+	print(f'{method}_{names[-1]}_time', end="")
+print()
+
+qiskit_analyzers = [QiskitAnalyzer(method, ignore_errors=True) for method in qiskit_methods]
+pytket_analyzers = [PytketAnalyzer(method, ignore_errors=True) for method in pytket_methods]
+
+for qubits in qubit_counts:
+	cnots = qubits * 10
+	arcs = [f(qubits) for f in arcfs]
+	random.seed(random_seed)
+	q_circ = [random_CX_circuit(qubits, cnots, "qiskit") for _ in range(runs)]
+	random.seed(random_seed)
+	p_circ = [random_CX_circuit(qubits, cnots, "pytket") for _ in range(runs)]
+	for i in range(runs):
+		qiskit_results = [a.analyse(q_circ[i], arcs) for a in qiskit_analyzers]
+		pytket_results = [a.analyse(p_circ[i], arcs) for a in pytket_analyzers]
+		print(qubits, end="")
+		for results in qiskit_results + pytket_results:
+			for r in results:
+				print(f',{r.gate_depth},{r.gate_count},{r.transpile_time}', end="")
+		print()
